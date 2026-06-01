ea7fb4c6cb83ca6830edf86a191542ed99673196
Stefan Ilic <ilicstefan272@gmail.com>
2026-06-01 02:25:11 +0200

    Struktura foldera i modeli; Saro proveri interfejse da li imaju smisla

---
+++ src/models/types.ts
@@ -0,0 +1,70 @@
+export interface Korisnik {
+  ime: string;
+  prezime: string;
+  email: string;
+  telefon: string;
+  adresa: string;
+}
+
+export interface Ljubimac {
+  id: number;
+  ime: string;
+  vrsta: string;
+  rasa: string;
+  starost: number;
+  slika?: string;
+}
+
+export interface Usluga {
+  id: number;
+  naziv: string;
+  opis: string;
+  cena: number;
+  kategorija: string;
+  vrsteLjubimaca: string[];
+  ocena: number;
+  brojRecenzija: number;
+  slika: string;
+}
+
+export interface Termin {
+  id: number;
+  datum: string;
+  usluga: string;
+  ljubimac: string;
+  status: "Potvrdjeno" | "Na cekanju" | "Otkazano";
+}
+
+export interface Rezencija {
+  autor: string;
+  ocena: number;
+  komentar: string;
+  datum: string;
+}
+
+export class TerminManager {
+  private termini: Termin[];
+
+  constructor(termini: Termin[]) {
+    this.termini = termini;
+  }
+
+  getTermini(): Termin[] {
+    return this.termini;
+  }
+
+  otkaziTermin(id: number): Termin[] {
+    this.termini = this.termini.filter((t) => t.id !== id);
+    return this.termini;
+  }
+
+  dodajTermin(termin: Omit<Termin, "id">): Termin {
+    const novi: Termin = { ...termin, id: Date.now() };
+    this.termini.push(novi);
+    return novi;
+  }
+
+  getAktivneTermine(): Termin[] {
+    return this.termini.filter((t) => t.status !== "Otkazano");
+  }
+}
