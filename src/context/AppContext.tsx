cb94bc87705917567126224a5ff4863f593ba6f3
Stefan Ilic <ilicstefan272@gmail.com>
2026-06-01 02:52:24 +0200

    Pocetni podaci za termine

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -9,3 +9,25 @@ interface AppContextType {
 }
 
 const AppContext = createContext<AppContextType | null>(null);
+
+const pocetniLjubimci: Ljubimac[] = [
+  { id: 1, ime: "Reks", vrsta: "Pas", rasa: "Labrador", starost: 4 },
+  { id: 1, ime: "Maza", vrsta: "Mačka", rasa: "Persijska", starost: 2 },
+];
+
+const pocetniTermini: Termin[] = [
+  {
+    id: 1,
+    datum: "5.12.2026.",
+    usluga: "Redovni pregled",
+    ljubimac: "Reks",
+    status: "Potvrđeno",
+  },
+  {
+    id: 2,
+    datum: "22.01.2027.",
+    usluga: "Vakcinacija",
+    ljubimac: "Maza",
+    status: "Na čekanju",
+  },
+];
