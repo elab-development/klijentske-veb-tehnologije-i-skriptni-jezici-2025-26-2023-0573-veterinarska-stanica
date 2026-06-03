9caa5905baa7020701d39e6cfe4304bd7a127bce
Sara Jecmenica <jsaricaa27@gmail.com>
2026-06-04 00:01:26 +0200

    Dodate Navbar i Footer komponente

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import { AppProvider } from "./context/AppContext";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Zakazivanje from "./pages/Zakazivanje";
-
+import "./App.css";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/zakazivanje" element={<Zakazivanje />} />
-      </Routes>
-    </BrowserRouter>
+    <AppProvider>
+      <BrowserRouter>
+        <div className="app-wrapper">
+          <Navbar />
+          <div className="app-content">
+            <Routes>
+              <Route path="/zakazivanje" element={<Zakazivanje />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </AppProvider>
   );
 }
-
 export default App;
